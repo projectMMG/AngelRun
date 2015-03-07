25ec7562a08f2e148026551bd8b749e4fe8cab3c
CatBox <damian.bonggi@gmail.com>
2015-03-07 12:41:42 +0900

    prototype 2015-03-07
    -Map scrolling
    -Player Character Movement

---
+++ Assets/scripts/AngelAction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AngelAction : MonoBehaviour {
+	public float angelGravity = 1;
+	public float velotest = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			if(rigidbody2D.velocity.y < 0)
+			{
+				rigidbody2D.velocity = new Vector2(0,0);
+			}
+			rigidbody2D.gravityScale = -angelGravity;
+		}
+		else
+		{
+			rigidbody2D.gravityScale = angelGravity;
+		}
+		velotest = rigidbody2D.velocity.y;
+	
+	}
+}
---
+++ Assets/scripts/BGAction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGAction : MonoBehaviour {
+	public float scrlSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(-0.01f*scrlSpeed,0,0);
+		
+		if(transform.localPosition.x < -20.0f)
+		{
+			transform.localPosition = new Vector3(20.0f,-0.5f,0);
+		}
+	
+	}
+}
