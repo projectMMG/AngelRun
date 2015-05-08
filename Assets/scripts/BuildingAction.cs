3429929b2481ecfd3db6f75f8d1221c9b47d2feb
Minue <roommy2@naver.com>
2015-05-08 19:35:36 +0900

    이미지 적용 및 수치 조정

--- Assets/scripts/BuildingAction.cs
+++ Assets/scripts/BuildingAction.cs
@@ -8,7 +8,7 @@ public class BuildingAction : MonoBehaviour {
 	void Start () {
 		//초기설정
 		transform.parent = GameObject.Find("02_BuildingList").transform;
-		transform.position = new Vector2(128.0f,5.1f);
+		transform.position = new Vector2(128.0f,5.0f);
 		rigidbody2D.velocity = new Vector2(-scrlSpeed,0);
 	
 	}
--- Assets/scripts/HouseAction.cs
+++ Assets/scripts/HouseAction.cs
@@ -8,7 +8,7 @@ public class HouseAction : MonoBehaviour {
 	void Start () {
 		//초기설정
 		transform.parent = GameObject.Find("02_BuildingList").transform;
-		transform.position = new Vector2(128.0f,5.1f);
+		transform.position = new Vector2(128.0f,5.0f);
 		rigidbody2D.velocity = new Vector2(-scrlSpeed,0);
 	
 	}
